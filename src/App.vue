ca0de9c489c7b8b59ec1125ce1fc0e5fadba39be
ataul443 <ataul.one@gmail.com>
2018-11-07 15:37:11 +0530

    first version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-  <app-header></app-header>
+  <div>
+    <app-header v-bind:user="user"></app-header>
+    <app-banner v-bind:user="user"></app-banner>
+    <home-page-body v-bind:user="user"></home-page-body>
+    <app-footer></app-footer>
+  </div>
 
 </template>
 
 <script>
 import Header from "./components/Header";
+import Banner from "./components/Banner";
+import HomePageBody from "./components/HomePage/HomePageBody";
+import Footer from "./components/Footer";
+
 export default {
   name: "App",
   components: {
-    "app-header": Header
+    "app-header": Header,
+    "app-banner": Banner,
+    "home-page-body": HomePageBody,
+    "app-footer": Footer
+  },
+  data: function data() {
+    return {
+      user: {
+        name: "Shekh Ataul",
+        role: "Software Developer"
+      }
+    };
   }
 };
 </script>
 
 <style>
+html {
+  font-size: 16px;
+}
 </style>
